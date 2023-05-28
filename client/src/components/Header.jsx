ca44cf9be49c3a7972b7304da57edab99ccdefb1
Lime <alsayyd.medo72@gmail.com>
2023-05-28 22:46:57 +0300

    last

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,9 +7,12 @@ import Logo from "../img/logo.png";
 import SearchForm from "./SearchForm";
 import Cart from "./Cart";
 import { CartContext } from "../context/CartContext";
+
 const Header = () => {
   const { isOpen, setIsOpen, itemsAmount } = useContext(CartContext);
   const [catNavMobile, setCatNavMobile] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   return (
     <header className="bg-primary py-6 fixed w-full top-0 z-40 lg:relative xl:mb-[30px]">
       <div className=" container mx-auto">
@@ -33,9 +36,53 @@ const Header = () => {
           <div className="hidden w-full xl:flex xl:max-w-[734px]">
             <SearchForm />
           </div>
-          <div className="flex items-center gap-x-[10px]">
-            <div className="hidden xl:flex uppercase">
-              need help ? +2001283845336
+          <div className="flex items-center gap-x-[10px] z-50">
+            <div className="relative inline-block text-left">
+              <div>
+                <button
+                  onClick={() => setDropdownOpen(!dropdownOpen)}
+                  className="flex items-center text-sm font-medium text-black  hover:border-gray-300 bg-accent p-2 rounded transition duration-150 ease-in-out"
+                >
+                  <span>Profile</span>
+                </button>
+              </div>
+              <div
+                className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="menu-button"
+                tabIndex="-1"
+                style={{ display: dropdownOpen ? "block" : "none" }}
+              >
+                <div className="py-1" role="none">
+                  <Link
+                    className="flex items-center  px-2 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                    role="menuitem"
+                  >
+                    <img
+                      className="h-8 w-8 rounded-full mr-2"
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScJXWdvwDZC0RF_VSzzP8aXSX9Sc_VPAtuew&usqp=CAU"
+                      alt=""
+                    />
+                    <span className="flex-1">Medo</span>
+                  </Link>
+                  <span className="flex items-center justify-center  text-gray-700/75">
+                    medo@gmail.com
+                  </span>
+                  <span className="flex items-center justify-center  text-gray-700/75">
+                    +2001210101010
+                  </span>
+                </div>
+
+                <div className="py-1" role="none">
+                  <Link
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                    role="menuitem"
+                  >
+                    Sign out
+                  </Link>
+                </div>
+              </div>
             </div>
             <div
               onClick={() => setIsOpen(!isOpen)}
@@ -58,9 +105,9 @@ const Header = () => {
             </div>
           </div>
         </div>
-        {/* <div className="xl:hidden">
+        <div className="xl:hidden">
           <SearchForm />
-        </div> */}
+        </div>
       </div>
     </header>
   );
